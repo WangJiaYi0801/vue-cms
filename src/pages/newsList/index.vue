b5a5a247e55d05688a6d5aa6bdb4254026d41e18
WangJiaYi0801 <291122760@qq.com>
2019-02-13 15:18:42 +0800

    newsList page

---
+++ src/pages/newsList/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="news-list-component">
+        newslist
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// @就是src目录
-// import HelloWorld from '@/components/HelloWorld'
+
 import homeComponent from '../pages/home'
 import memberComponent from '../pages/member'
 import shopcarComponent from '../pages/shopcar'
 import searchComponent from '../pages/search'
+import newsListComponent from '../pages/newsList'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [ // 路由的匹配规则
-    { path: '/', redirect: '/home' },
-    { path: '/home', component: homeComponent },
-    { path: '/member', component: memberComponent },
-    { path: '/shopcar', component: shopcarComponent },
-    { path: '/search', component: searchComponent },
-    // {
-    //   // 当用户访问 / 目录时  会在App.vue的router-view组件的地方显示HelloWorld组件
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // }
-  ],
-  linkActiveClass: 'mui-active' // 类库
+    routes: [
+        { path: '/', redirect: '/home' },
+        { path: '/home', component: homeComponent },
+        { path: '/member', component: memberComponent },
+        { path: '/shopcar', component: shopcarComponent },
+        { path: '/search', component: searchComponent },
+        { path: '/home/newsList', component: newsListComponent },
+    ],
+    linkActiveClass: 'mui-active' // 类库
 })
