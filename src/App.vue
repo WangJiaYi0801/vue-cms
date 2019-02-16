62457dc1ca13eeb724bfa49dc5111d176a45f6ae
WangJiaYi0801 <291122760@qq.com>
2019-02-16 21:40:24 +0800

    finish goodsInfo page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <mt-header fixed title="黑马程序员高端项目"></mt-header>
-    <router-view/>
+    <transition name="app">
+      <router-view/>
+    </transition>
     <nav class="mui-bar mui-bar-tab">
       <router-link class="mui-tab-item1" to="/home">
         <span class="mui-icon mui-icon-home"></span>
@@ -13,7 +15,7 @@
       </router-link>
       <router-link class="mui-tab-item1" to="/shopcar">
         <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
-          <span class="mui-badge">0</span>
+          <span class="mui-badge" id="badge">0</span>
         </span>
         <span class="mui-tab-label">购物车</span>
       </router-link>
@@ -40,17 +42,17 @@ export default {
     z-index: 999;
   }
 }
-.v-enter {
+.app-enter {
   opacity: 0;
   transform: translateX(100%);
 }
-.v-leave-to {
+.app-leave-to {
   opacity: 0;
   transform: translateX(-100%);
   position: absolute;
 }
-.v-enter-active,
-.v-leave-active {
+.app-enter-active,
+.app-leave-active {
   transition: all 0.5s ease;
 }
 /* .router-link-active {
--- src/pages/goodsInfo/index.vue
+++ src/pages/goodsInfo/index.vue
@@ -1,54 +1,193 @@
 <template>
-    <div class="goodsinfo-container">
-        
-        <!-- 轮播图 -->
-        <div class="mui-card">
-            <div class="mui-card-content">
-                <div class="mui-card-content-inner">
-                    <swiper :banners="lunbotu"></swiper>
-                </div>
-            </div>
-        </div>
+  <div class="goodsinfo-container">
+    <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+      <div class="ball" v-show="ballFlag" ref="ball"></div>
+    </transition>
 
-        <!-- 商品购买区域 -->
-        <div class="mui-card">
-            <div class="mui-card-header">{{goodsinfo.title}} </div>
-            <div class="mui-card-content">
-                <div class="mui-card-content-inner">
-                    <p class="price">
-                        市场价:
-                        <del>¥{{goodsinfo.market_price}} </del>
-                        &nbsp;&nbsp;销售价:
-                        <span class="now_price">¥{{goodsinfo.sell_price}} </span>
-                    </p>
-                    <div class="numbox">
-                        <span class="title">购买数量:</span>
-                        <input type="button" @click="buyCount >=2 && buyCount--" value="-" :disabled="buyCount == 1">
-                        <input type="number" @change="filterMaxCount" v-model="buyCount">
-                        <input type="button" @click="buyCount < goodsinfo.stock_quantity && buyCount++" value="+" :disabled="buyCount == goodsinfo.stock_quantity">
-                    </div>
-                    <p>
-                        <mt-button type="primary" size="small">立即购买</mt-button>
-                        <mt-button type="danger" size="small" @click="addToShopCar">加入购物车</mt-button>
-                    </p>
-                </div>
-            </div>
+    <!-- 轮播图 -->
+    <div class="mui-card">
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner">
+          <mt-swipe :auto="0">
+            <mt-swipe-item v-for="(item, index) in lunbotu" :key="index">
+                <img :src="item.src">
+            </mt-swipe-item>
+          </mt-swipe>
         </div>
-
-        <!-- 商品参数区域 -->
-        <div class="mui-card">
-            <div class="mui-card-header">商品参数</div>
-            <div class="mui-card-content">
-                <div class="mui-card-content-inner">
-                    <p>商品货号:{{goodsinfo.goods_no}} </p>
-                    <p>库存情况:{{goodsinfo.stock_quantity}} </p>
-                    <p>上架时间:{{goodsinfo.add_time | dateFormat}} </p>
-                </div>
-            </div>
-            <div class="mui-card-footer">
-                <mt-button type="primary" size="large" plain @click="goDesc(id)">图文介绍</mt-button>
-                <mt-button type="danger" size="large" plain @click="goComment(id)">商品评论</mt-button>
-            </div>
-        </div>
+      </div>
     </div>
+
+    <!-- 商品购买区域 -->
+    <div class="mui-card">
+      <div class="mui-card-header">{{goodsinfo.title}}</div>
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner">
+          <p class="price">
+            市场价:
+            <del>¥{{goodsinfo.market_price}}</del>
+            &nbsp;&nbsp;销售价:
+            <span class="now_price">¥{{goodsinfo.sell_price}}</span>
+          </p>
+          <div class="numbox">
+            <span class="title">购买数量:</span>
+            <input
+              type="button"
+              @click="buyCount >=2 && buyCount--"
+              value="-"
+              :disabled="buyCount == 1"
+            >
+            <input type="number" @change="filterMaxCount" v-model="buyCount">
+            <input
+              type="button"
+              @click="buyCount < goodsinfo.stock_quantity && buyCount++"
+              value="+"
+              :disabled="buyCount == goodsinfo.stock_quantity"
+            >
+          </div>
+          <p>
+            <mt-button type="primary" size="small">立即购买</mt-button>
+            <mt-button type="danger" size="small" @click="addToShopCar">加入购物车</mt-button>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- 商品参数区域 -->
+    <div class="mui-card">
+      <div class="mui-card-header">商品参数</div>
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner">
+          <p>商品货号:{{goodsinfo.goods_no}}</p>
+          <p>库存情况:{{goodsinfo.stock_quantity}}</p>
+          <p>上架时间:{{goodsinfo.add_time | dateFormat}}</p>
+        </div>
+      </div>
+      <div class="mui-card-footer">
+        <mt-button type="primary" size="large" plain @click="goDesc(id)">图文介绍</mt-button>
+        <mt-button type="danger" size="large" plain @click="goComment(id)">商品评论</mt-button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      lunbotu: [],
+      goodsinfo: {},
+      ballFlag: false,
+      selectedCount: 1,
+      buyCount: 1
+    };
+  },
+  created() {
+    this.getLunbotu();
+    this.getGoodsInfo();
+  },
+  methods: {
+    getLunbotu() {
+      this.$http.get("getthumimages/" + this.id).then(result => {
+        if (result.body.status === 0) {
+          this.lunbotu = result.body.message;
+        }
+      });
+    },
+    getGoodsInfo() {
+      this.$http.get("goods/getinfo/" + this.id).then(result => {
+        if (result.body.status === 0) {
+          this.goodsinfo = result.body.message[0];
+        }
+      });
+    },
+    filterMaxCount() {
+      if (this.buyCount > this.goodsinfo.stock_quantity) {
+        this.buyCount = this.goodsinfo.stock_quantity;
+      }
+    },
+    goDesc(id) {
+      this.$router.push("/home/goodsDesc/" + id);
+    },
+    goComment(id) {
+      this.$router.push("/home/goodsComment/" + id);
+    },
+    addToShopCar() {
+      this.ballFlag = !this.ballFlag;
+    },
+    beforeEnter: el => {
+      el.style.transform = "translate(0,0)";
+    },
+    enter(el, done) {
+      el.offsetWidth;
+      const ballPosition = this.$refs.ball.getBoundingClientRect();
+      const badgePosition = document
+        .getElementById("badge")
+        .getBoundingClientRect();
+      const xDist = badgePosition.left - ballPosition.left;
+      const yDist = badgePosition.top - ballPosition.top;
+      el.style.transform = `translate(${xDist}px,${yDist}px)`;
+      el.style.transform = "all 0.5s cubic-bezier(.4,-0.3,1,.68)";
+      done();
+    },
+    afterEnter(el) {
+      this.ballFlag = !this.ballFlag;
+    },
+    getSelectedCount(count) {
+      this.selectedCount = count;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.goodsinfo-container {
+  background-color: #eee;
+  overflow: hidden;
+  .mint-swipe{
+      height: 180px;
+      img{
+          width: 100%;
+      }
+  }
+  .numbox {
+    display: flex;
+    height: 50px;
+    align-items: center;
+    input {
+      width: 30px;
+      height: 30px;
+      padding: 0;
+      margin: 0;
+      text-align: center;
+      &[type="number"] {
+        border-left: 0;
+        border-right: 0;
+        font-size: 12px;
+        color: gray;
+      }
+    }
+  }
+  .now_price {
+    color: red;
+    font-size: 16px;
+    font-weight: bold;
+  }
+  .mui-card-footer {
+    display: block;
+    button {
+      margin: 15px 0;
+    }
+  }
+  .ball {
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: red;
+    position: absolute;
+    z-index: 99;
+    top: 390px;
+    left: 146px;
+  }
+}
+</style>
